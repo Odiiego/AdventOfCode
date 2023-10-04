25a7608ffcc99ec56de6dec24df9cc1b62eb674f
Odiiego <odiegopaula@gmail.com>
2023-10-04 02:08:03 -0300

    Primeiro Desafio [2/2]

--- 2015/1.py
+++ 2015/1.py
@@ -13,4 +13,17 @@ def floorCode(input):
         return count
 
 
-floorCode(input)
+def secondFloorCode(input):
+    count = 0
+    for data in input:
+        for n, char in enumerate(data):
+            if char == "(":
+                count += 1
+            elif char == ")":
+                count -= 1
+                if count == -1:
+                    print(n+1)
+                    return(n+1)
+
+
+secondFloorCode(input)

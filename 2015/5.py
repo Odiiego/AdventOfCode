2dad673407830fee3ce3737bfd690912bb7b06ad
Odiiego <odiegopaula@gmail.com>
2023-11-13 02:08:48 -0300

    Quinto Desafio [2/2]

--- 2015/5.py
+++ 2015/5.py
@@ -21,3 +21,18 @@ def niceStringDetector(input):
         if not forbiddenLetters and doubleLetters and vowelCount >= 3:
             total += 1
     print(total)
+
+def improvedNiceStringDetector(input):
+    total = 0
+    for string in input:
+        repeatedPair = False
+        splitRepeatition = False
+        for n in range(len(string)):
+            if n+2 < len(string) and string[n] != string[n+1] and string[n] == string[n+2]:
+                splitRepeatition = True
+            for i in range(n + 2, len(string)):
+                    if string[n:n+2] == string[i:i+2]:
+                        repeatedPair = True
+        if repeatedPair and splitRepeatition:
+            total += 1
+    print(total)

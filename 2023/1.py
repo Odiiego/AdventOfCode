169feee4a8babc69be04106548637c1871b0be7b
Odiiego <odiegopaula@gmail.com>
2023-12-02 13:20:59 -0300

    Primeiro Desafio [2/2]

--- 2023/1.py
+++ 2023/1.py
@@ -16,3 +16,34 @@ def improvedCalibration(input):
         total += int(firstDigit + lastDigit)
         break
   print(total)
+
+
+def reallyImprovedCalibration(input):
+  total = 0
+  numList = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+  for line in input:
+    firstDigit = {
+      "digit": None,
+      "position": len(line)
+    }
+    lastDigit = {
+      "digit": None,
+      "position": -1
+    }
+
+    for index, num in enumerate(numList):
+      if firstDigit["position"] > line.find(num) and line.find(num) >= 0:
+        firstDigit = {
+          "digit": str(index % 10),
+          "position": line.find(num)
+        }
+
+      if lastDigit["position"] < line.rfind(num):
+        lastDigit = {
+          "digit": str(index % 10),
+          "position": line.rfind(num)
+        }
+    
+    total += int(firstDigit["digit"] + lastDigit["digit"])
+  print(f"total: {total}")

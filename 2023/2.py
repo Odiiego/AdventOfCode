62415c8c35870f18aa75168d19195e6a69678239
Odiiego <odiegopaula@gmail.com>
2023-12-04 01:44:24 -0300

    Segundo Desafio [1/2]

---
+++ 2023/2.py
@@ -0,0 +1,25 @@
+input = open("2.txt", "r")
+
+def validGameSum(input):
+  total = 0
+  input = [ x.strip().split(": ")[1].split("; ") for x in input]
+
+  for index, game in enumerate(input):
+    sets = [set.split(", ") for set in game]
+    validGame = True
+    
+    for set in sets:
+      counter = {
+        "red": 0,
+        "green": 0,
+        "blue": 0
+      }
+
+      for color in set:
+        color = color.split(" ")
+        counter[color[1]] += int(color[0])
+
+      if counter["red"] > 12 or counter["green"] > 13 or counter["blue"] > 14:
+        validGame = False
+    total += index+1 if validGame else 0 
+  print(total)

8b288b61332974897cf59efd65799aa0556cb6d1
Odiiego <odiegopaula@gmail.com>
2023-12-04 01:58:56 -0300

    Segundo Desafio [2/2]

--- 2023/2.py
+++ 2023/2.py
@@ -23,3 +23,24 @@ def validGameSum(input):
         validGame = False
     total += index+1 if validGame else 0 
   print(total)
+
+
+def checkMinimumSet(input):
+  total = 0
+  input = [ x.strip().split(": ")[1].split("; ") for x in input]
+
+  for game in input:
+    sets = [set.split(", ") for set in game]
+    counter = {
+      "red": 0,
+      "green": 0,
+      "blue": 0
+    }
+
+    for set in sets:
+      for color in set:
+        color = color.split(" ")
+        counter[color[1]] = int(color[0]) if counter[color[1]] < int(color[0]) else counter[color[1]]
+    
+    total += counter["red"] * counter["green"] * counter["blue"]
+  print(total)
